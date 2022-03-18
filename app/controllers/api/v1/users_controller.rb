c85d3ea2e0b9d30a28e065a75a4b690cb1dae9d1
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-17 23:33:40 -0400

    add User create i API controller and user validation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user@user) }, status: :created
+        else
+            render json: {
+                error: "failed to create user",
+            },
+            status: :unprocessable_entity
+    end
+end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :avatar)
+    end
 end
