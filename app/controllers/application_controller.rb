817f0dde9fcf867acab52397c1cadadf1df6b18b
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-18 00:43:43 -0400

    add auth header and change decode_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,17 @@ class ApplicationController < ActionController::API
         JWT.encode(payload, 'my_s3cre3t')
     end
 
-    def decoded_token(token)
-        JWT.decode(token, 'my_s3cr3t')[0] 
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+       if auth_header
+        token = auth_header.split(' ')[1]
+        begin
+            JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+        rescue JWT::DecodeError
+            nill
+        end
     end
 end
