b91f75a61f085762cdc2743d46a09a98de1cd920
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-18 00:37:35 -0400

    add encode and decoded JWT instants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode(payload, 'my_s3cre3t')
+    end
+
+    def decoded_token(token)
+        JWT.decode(token, 'my_s3cr3t')[0] 
+    end
 end
