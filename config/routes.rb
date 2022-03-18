acbde7fa2c163bb254f70bc9b9cee1257d0f2a56
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-17 23:48:03 -0400

    add route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
  
   resources :contacts
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resource :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
