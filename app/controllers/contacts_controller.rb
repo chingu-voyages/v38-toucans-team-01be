19663b91aeeab94d9d61864f048426ac3a47b5f1
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-03-19 15:18:26 -0500

    add Api::V1:: to contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,4 @@
-class ContactsController < ApplicationController
+class Api::V1::ContactsController < ApplicationController
   before_action :set_contact, only: [:show, :update, :destroy]
 
   # GET /contacts
