8781858eb43c9966063b991638f8b15e3e4654b0
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-03-19 19:48:30 -0500

    show relationship between users and contacts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :users, only: [:create]
       post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
+      get '/profile', to: 'users#profile' do
+        resources :contacts
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
