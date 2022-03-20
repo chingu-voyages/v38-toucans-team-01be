c64582d8247f446f986ed04157a0068e93d1ddbe
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-03-19 19:19:01 -0500

    fix syntax error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
         if @user.valid?
             @token = encode_token(user_id: @user.id)
             render json: {
-                 user: UserSerializer.new(@user) }, 
+                 user: UserSerializer.new(@user), 
                  jwt: @token,
                 },
                 status: :created
