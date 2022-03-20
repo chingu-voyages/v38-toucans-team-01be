87728160676c2e9e2e501ce3035d99ad99a15e67
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-20 01:52:42 -0400

    fix user_id foreign key in contact and add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username:"ruby", password_digest:"123456", avatar: "super")
+
+Contact.create(name: "lexy", birthday: '1983-11-30', number:"1234567879", email:"email@email.com", address: "New York, NY", notes: "I have a long note here", user_id: user.id)
+
+user2 = User.create(username:"wax", password_digest:"123456", avatar: "badman")
+
+Contact.create(name: "react", birthday: '1995-12-25', number:"1234567879", email:"example@example.com", address: "Brooklyn, NY", notes: "Another note testing", user_id: user.id)
